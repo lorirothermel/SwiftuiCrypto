321b70b352bf992281df34fc30bfea8fa8e918f5
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-01 00:22:23 -0400

    Initial Commit

---
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by Lori Rothermel on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
