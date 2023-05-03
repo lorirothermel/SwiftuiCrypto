b528bc1f8786f0b57b561c1123fb1d4b39e6e105
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-03 16:12:47 -0400

    Day 4

--- SwiftuiCrypto/Services/CoinImageService.swift
+++ SwiftuiCrypto/Services/CoinImageService.swift
@@ -30,10 +30,8 @@ class CoinImageService {
     private func getCoinImage() {
         if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
             image = savedImage
-            print("Retrieved image from File Manager")
         } else {
             downloadCoinImage()
-            print("Downloading Image Now")
         }
     }
     
